b3628fdb3cb77e39f243f4aae4b63b846a60eb73
charliebrown364 <364charliebrown@gmail.com>
2021-01-27 05:34:02 +0000

    Finished Assignment 49, which was refactoring my tree class so that each node has an index.

--- src/tree.py
+++ src/tree.py
@@ -1,34 +1,56 @@
 class Node():
-    def __init__(self, value):
+    def __init__(self, value, index):
         self.value = value
         self.children = []
+        self.index = index
 
-# nodes_breadth_first and nodes_depth_first aren't perfect, because the order of each node's children in get_chilren is off (b/c the order in the original edges array is weird)
+# nodes_breadth_first and nodes_depth_first might not work because they are still based on an input of the value, not the index
 
 class Tree():
 
-    def __init__(self, edges):
+    def __init__(self, edges, node_values):
         self.edges = edges
-        self.root = Node(self.get_root()[0])
+        self.tops = [pair[0] for pair in self.edges]
+        self.bottoms = [pair[1] for pair in self.edges]
+        
+        self.node_values = node_values
+        self.edges_values = [[self.node_values[i] for i in pair] for pair in self.edges]        
+        
+        self.root = self.get_root()
     
+    # self.get_children(4) = [6, 8, 0]
+
     def get_children(self, parent):
-        return [Node(pair[1]) for pair in self.edges if pair[0] == parent]
-    def get_parents(self, child):
-        return [Node(pair[0]) for pair in self.edges if pair[1] == child]
-    def get_root(self):
-        children = [pair[1] for pair in self.edges]
         ans = []
         for pair in self.edges:
-            if (pair[0] not in children) and (pair[0] not in ans):
-                ans.append(pair[0])
+            if pair[0] == parent:
+                node_index = pair[1]
+                node_value = self.node_values[node_index]
+                node = Node(node_value, node_index)
+                ans.append(node)
         return ans
+    
+    def get_parents(self, child):
+        return [Node(pair[0], self.node_values.index(pair[0])) for pair in self.edges_values if pair[1] == child]
+    
+    def get_root(self):
+        
+        for top in self.tops:
+            if top not in self.bottoms:
+                head_index = top
+
+        head_value = self.node_values[head_index]
+        head_node = Node(head_value, head_index)
+        return head_node
+    
     def build_from_edges(self):
         
         node_array = [self.root]
         while len(node_array) != 0:
+            
             child_array = []
             for node in node_array:
-                new_children = self.get_children(node.value)
+                new_children = self.get_children(node.index)
                 node.children = new_children
                 child_array += new_children
             
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -2,14 +2,145 @@ import sys
 sys.path.append('src')
 from tree import Tree
 
-edges = [('a','c'), ('e','g'), ('e','i'), ('e','a'), ('d','b'), ('a','d'), ('d','f'), ('f','h'), ('d','j'), ('d','k')]
-tree = Tree(edges)
+node_values = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+edges = [(0,2), (4,6), (4,8), (4,0), (3,1), (0,3), (3,5), (5,7), (3,9), (3,10)]
+
+tree = Tree(edges, node_values)
 tree.build_from_edges()
 
-nodes = tree.nodes_breadth_first()
-assert [node.value for node in nodes] == ['e','g','i','a','c','d','b','f','j','k','h'], [node.value for node in nodes]
+"""
+The indices of the nodes are as follows:
 
-nodes = tree.nodes_depth_first()
-assert [node.value for node in nodes] == ['e','g','i','a','c','d','b','f','h','j','k'], [node.value for node in nodes]
+    4
+   /|\
+  0 8 6
+ /|
+2 3__
+ /|\ \
+1 9 5 10
+    |
+    7
+
+The values of the nodes are as follows:
+
+    e
+   /|\
+  a i g
+ /|
+c d 
+ /|\\
+b j fk
+    |
+    h
+"""
+
+assert tree.root.value == 'e'
+assert tree.root.index == 4
+
+# Note: the following tests use sets {} rather than lists [].
+# This way, you don't have to worry about order.
+
+children = set(tree.root.children)
+
+grandchildren = set([])
+for child in children:
+    grandchildren = grandchildren.union(set(child.children))
+
+great_grandchildren = set([])
+for grandchild in grandchildren:
+    great_grandchildren = great_grandchildren.union(set(grandchild.children))
+
+great_great_grandchildren = set([])
+for great_grandchild in great_grandchildren:
+    great_great_grandchildren = great_great_grandchildren.union(set(great_grandchild.children))
+
+assert {node.index for node in children} == {0, 8, 6}
+assert {node.value for node in children} == {'a', 'i', 'g'}
+
+assert {node.index for node in grandchildren} == {2, 3}
+assert {node.value for node in grandchildren} == {'c', 'd'}
+
+assert {node.index for node in great_grandchildren} == {1, 9, 5, 10}
+assert {node.value for node in great_grandchildren} == {'b', 'j', 'f', 'k'}
+
+assert {node.index for node in great_great_grandchildren} == {7}
+assert {node.value for node in great_great_grandchildren} == {'h'}
+
+node_values = ['a', 'b', 'a', 'a', 'a', 'b', 'a', 'b', 'a', 'b', 'b']
+
+"""
+This means that the nodes will be as follows:
+- the node with index 0 will have value 'a'
+- the node with index 1 will have value 'b'
+- the node with index 2 will have value 'a'
+- the node with index 3 will have value 'a'
+- the node with index 4 will have value 'a'
+- the node with index 5 will have value 'b'
+- the node with index 6 will have value 'a'
+- the node with index 7 will have value 'b'
+- the node with index 8 will have value 'a'
+- the node with index 9 will have value 'b'
+- the node with index 10 will have value 'b'
+"""
+
+edges = [(0,2), (4,6), (4,8), (4,0), (3,1), (0,3), (3,5), (5,7), (3,9), (3,10)]
+
+tree = Tree(edges, node_values)
+tree.build_from_edges()
+
+"""
+The indices of the nodes are as follows:
+
+    4
+   /|\
+  0 8 6
+ /|
+2 3__
+ /|\ \
+1 9 5 10
+    |
+    7
+
+The values of the nodes are as follows:
+
+    a
+   /|\
+  a a a
+ /|
+a a 
+ /|\\
+b b bb
+    |
+    b
+"""
+
+assert tree.root.value == 'a'
+assert tree.root.index == 4
+
+children = set(tree.root.children)
+
+grandchildren = set([])
+for child in children:
+    grandchildren = grandchildren.union(set(child.children))
+
+great_grandchildren = set([])
+for grandchild in grandchildren:
+    great_grandchildren = great_grandchildren.union(set(grandchild.children))
+
+great_great_grandchildren = set([])
+for great_grandchild in great_grandchildren:
+    great_great_grandchildren = great_great_grandchildren.union(set(great_grandchild.children))
+
+assert {node.index for node in children} == {0, 8, 6}
+assert {node.value for node in children} == {'a', 'a', 'a'}
+
+assert {node.index for node in grandchildren} == {2, 3}
+assert {node.value for node in grandchildren} == {'a', 'a'}
+
+assert {node.index for node in great_grandchildren} == {1, 9, 5, 10}
+assert {node.value for node in great_grandchildren} == {'b', 'b', 'b', 'b'}
+
+assert {node.index for node in great_great_grandchildren} == {7}
+assert {node.value for node in great_great_grandchildren} == {'b'}
 
 print("passed all")
