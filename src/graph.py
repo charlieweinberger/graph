a0120d496731e039be499b72b5356eb18120260c
charliebrown364 <364charliebrown@gmail.com>
2021-02-13 07:42:03 +0000

    Finished Assignment 54-1, which was creating the set_breadth_first_distance_and_previous(), calc_distance(), and calc_shortest_path() methods.

--- src/graph.py
+++ src/graph.py
@@ -2,16 +2,27 @@ class Node():
     def __init__(self, index):
         self.index = index
         self.value = None
+        self.distance = None
+        self.previous = None
         self.neighbors = []
+
+# might have to redo everything so that the class sorts by looking at nodes in self.nodes, not indices
 
 class Graph():
 
     def __init__(self, edges):
         self.edges = edges
         self.nodes = []
+        self.indices = []
     
-    def indices(self, input_list):
+    def find_indices(self, input_list):
         return [node.index for node in input_list]
+    
+    def same_node_as(self, node1, node2):
+        return True if node1.index == node2.index else False
+
+    def if_node_in(self, node, node_list):
+        return True if node.index in self.find_indices(node_list) else False
     
     def get_neighbors(self, node):
         neighbors = []
@@ -37,22 +48,20 @@ class Graph():
             for index in pair:
                 neighbor_index = [num for num in pair if num != index][0]
                 self.nodes[index].neighbors.append(neighbor_index)
+        
+        self.indices = self.find_indices(self.nodes)
 
-    def get_nodes_breadth_first(self, starting_index):
+    def get_nodes_breadth_first(self, starting_node_index):
 
-        queue = [self.nodes[starting_index]]
+        starting_node = self.nodes[starting_node_index]
+        queue = [starting_node]
         visited = []
         
         while queue != []:
 
             current_node = queue[0]
             current_neighbors = self.get_neighbors(current_node)
- 
-            unvisited_neighbors = []
-            for neighbor in current_neighbors:
-                queue_plus_visited = self.indices(queue) + self.indices(visited)
-                if neighbor.index not in queue_plus_visited:
-                    unvisited_neighbors.append(neighbor)
+            unvisited_neighbors = [neighbor for neighbor in current_neighbors if not self.if_node_in(neighbor, queue + visited)]
 
             del queue[0]
             queue += unvisited_neighbors
@@ -60,9 +69,10 @@ class Graph():
         
         return visited
     
-    def get_nodes_depth_first(self, starting_index):
+    def get_nodes_depth_first(self, starting_node_index):
 
-        stack = [self.nodes[starting_index]]
+        starting_node = self.nodes[starting_node_index]
+        stack = [starting_node]
         visited = []
         
         while stack != []:
@@ -70,11 +80,7 @@ class Graph():
             current_node = stack[0]
             current_neighbors = self.get_neighbors(current_node)
 
-            unvisited_neighbors = []
-            for neighbor in current_neighbors:
-                stack_plus_visited = self.indices(stack) + self.indices(visited)
-                if neighbor.index not in stack_plus_visited:
-                    unvisited_neighbors.append(neighbor)
+            unvisited_neighbors = [neighbor for neighbor in current_neighbors if not self.if_node_in(neighbor, stack + visited)]
 
             for neighbor in unvisited_neighbors:
                 stack.insert(1, neighbor)
@@ -82,3 +88,47 @@ class Graph():
             visited.append(current_node)
         
         return visited
+    
+    def set_breadth_first_distance_and_previous(self, starting_node_index):
+
+        self.build_from_edges()
+        starting_node = self.nodes[starting_node_index]
+        starting_node.distance = 0
+        queue = [starting_node]
+        visited = []
+        
+        while queue != []:
+
+            current_node = self.nodes[queue[0].index]
+            current_neighbors = self.get_neighbors(current_node)
+            unvisited_neighbors = [neighbor for neighbor in current_neighbors if not self.if_node_in(neighbor, queue + visited)]
+
+            for neighbor in unvisited_neighbors:
+                for node_index in range(len(self.nodes)):
+                    self_node = self.nodes[node_index]
+                    if self.same_node_as(neighbor, self_node) and self_node.previous == None:
+                        self.nodes[node_index].previous = current_node
+                        self.nodes[node_index].distance = current_node.distance + 1
+
+            del queue[0]
+            queue += unvisited_neighbors
+            visited += [current_node]
+    
+    def calc_distance(self, starting_node_index, ending_node_index):
+        self.set_breadth_first_distance_and_previous(starting_node_index)
+        for node in self.nodes:
+            if self.same_node_as(node, self.nodes[ending_node_index]):
+                return node.distance
+    
+    def calc_shortest_path(self, starting_node_index, ending_node_index):
+
+        self.set_breadth_first_distance_and_previous(starting_node_index)
+        starting_node = self.nodes[starting_node_index]
+        current_node = self.nodes[ending_node_index]
+        visited = [current_node]
+
+        while not self.same_node_as(current_node, starting_node):
+            current_node = current_node.previous
+            visited.append(current_node)
+        
+        return self.find_indices(visited[::-1])
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -4,16 +4,19 @@ from graph import Graph
 
 edges = [(0,1),(1,2),(1,3),(3,4),(1,4),(4,5)]
 graph = Graph(edges)
-graph.build_from_edges()
 
-print("\n0 -- 1 -- 2")
-print("     | \ ")
-print("     3--4 -- 5\n")
+assert graph.calc_distance(0,4) == 2
+assert graph.calc_distance(5,2) == 3
+assert graph.calc_distance(0,5) == 3
+assert graph.calc_distance(4,1) == 1
+assert graph.calc_distance(3,3) == 0
 
-bf = graph.get_nodes_breadth_first(2)
-assert graph.indices(bf) == [2, 1, 0, 3, 4, 5]
-print("passed get_nodes_breadth_first")
+print("passed calc_distance tests")
 
-df = graph.get_nodes_depth_first(2)
-assert graph.indices(df) == [2, 1, 4, 5, 3, 0] # also [2, 1, 3, 4, 5, 0]
-print("passed get_nodes_depth_first")
+assert graph.calc_shortest_path(0,4) == [0, 1, 4], graph.calc_shortest_path(0,4)
+assert graph.calc_shortest_path(5,2) == [5, 4, 1, 2]
+assert graph.calc_shortest_path(0,5) == [0, 1, 4, 5]
+assert graph.calc_shortest_path(4,1) == [4, 1]
+assert graph.calc_shortest_path(3,3) == [3]
+
+print("passed calc_shortest_path tests")
